1570e0e9eba4edfaac2ffd0b6bbef87214f1f2f9
Aljaz Bozic <AljazBozic@users.noreply.github.com>
2020-10-26 11:05:11 +0100

    Update pwcnet.py

--- model/pwcnet.py
+++ model/pwcnet.py
@@ -1,6 +1,6 @@
-# Copyright (C) 2018 NVIDIA Corporation. 
+# Copyright (C) 2018 sniklaus. 
 # All rights reserved. 
-# Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
+# Licensed under the GNU General Public License v3.0 (https://github.com/sniklaus/pytorch-pwc/blob/master/LICENSE)
 
 import torch
 import torch.nn as nn
